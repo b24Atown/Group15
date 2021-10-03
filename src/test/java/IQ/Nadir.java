ee7d2b76ebbc46b88d89ed214544c4b2e416c5ca
Casper <tachpro2@gmail.com>
2021-10-03 19:46:27 -0400

    new

---
+++ src/test/java/IQ/Nadir.java
@@ -0,0 +1,9 @@
+package IQ;
+
+public class Nadir {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Nadir was here");
+        }
+    }
+}
