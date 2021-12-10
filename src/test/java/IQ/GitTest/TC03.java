d42227b2ebae60a7e2274aafc460352169b48feb
HP Laptop <katharina.slava@gmail.com>
2021-12-09 23:51:04 -0500

    Adding TC03-line 8

--- src/test/java/IQ/GitTest/TC03.java
+++ src/test/java/IQ/GitTest/TC03.java
@@ -7,5 +7,6 @@ public class TC03 {
         System.out.println("added line 7");
         System.out.println("adding a line 8");
         System.out.println("from sarah");
+        System.out.println("checking in");
     }
 }
