ca5b51217ad6df6794a62a273a73feb58ce91248
sarahDemir <sarahseymademir@gmail.com>
2021-12-09 23:43:55 -0500

    new changes

--- src/test/java/IQ/GitTest/TC03.java
+++ src/test/java/IQ/GitTest/TC03.java
@@ -3,7 +3,7 @@ package IQ.GitTest;
 public class TC03 {
 
     public static void main(String[] args) {
-        System.out.println("just created tc03");
+
         System.out.println("added line 7");
     }
 }
