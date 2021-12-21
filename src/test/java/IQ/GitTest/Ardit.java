bca68a7188cae8cf314d6de2f8054264893a184c
ArditPeni <arditpeni@gmail.com>
2021-12-21 14:51:34 -0500

    another one

--- src/test/java/IQ/GitTest/Ardit.java
+++ src/test/java/IQ/GitTest/Ardit.java
@@ -5,5 +5,6 @@ public class Ardit {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("making changes");
+        System.out.println("more changes");
     }
 }
