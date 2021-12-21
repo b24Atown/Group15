acd143ae6133bba90ea4d1052c22ad8acf5a454b
ArditPeni <arditpeni@gmail.com>
2021-12-21 15:11:56 -0500

    change to ardit2

--- src/test/java/IQ/GitTest/Ardit2.java
+++ src/test/java/IQ/GitTest/Ardit2.java
@@ -4,5 +4,6 @@ public class Ardit2 {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Hello" + "");
+        System.out.println("another line");
     }
 }
