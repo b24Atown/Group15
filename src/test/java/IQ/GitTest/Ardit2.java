77e626faefd66d821e41917576b0f20b0993ebce
ArditPeni <arditpeni@gmail.com>
2021-12-20 23:23:49 -0500

    trying

--- src/test/java/IQ/GitTest/Ardit2.java
+++ src/test/java/IQ/GitTest/Ardit2.java
@@ -3,5 +3,7 @@ package IQ.GitTest;
 public class Ardit2 {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Hello" +
+                "");
     }
 }
